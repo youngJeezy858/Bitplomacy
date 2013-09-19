ebe092774c473efe44e78f3f2622306477b0ada2
kfrank <kfrank@CSEESYSTEMS08.lcsee.wvu.edu>
2013-09-19 17:25:19 -0400

    initial commit

---
+++ Bitplomacy/src/Driver.java
@@ -0,0 +1,18 @@
+import com.erebos.engine.core.*;
+
+
+public class Driver extends EGame{
+
+	public Driver() {
+		super("Bitplomacy");
+		setShowFPS(true);
+		addCanvas(new Canvas(1));
+	}
+
+	public static void main(String[] args){
+		Driver d = new Driver();
+		
+		d.start(d, 1380, 831, false);
+	}
+	
+}
