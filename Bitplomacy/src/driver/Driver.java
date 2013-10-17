bc34376abc48a5de3713193a5c7afc1c313d13b9
nyxcharon <nyxcharon@gmail.com>
2013-10-17 14:48:13 -0400

    *Added code to extract native.zip from jar and set native path automatically on startup.

--- Bitplomacy/src/driver/Driver.java
+++ Bitplomacy/src/driver/Driver.java
@@ -1,13 +1,17 @@
 package driver;
 import java.io.File;
-import java.io.ObjectInputStream.GetField;
-
+import java.util.zip.ZipEntry;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.zip.ZipInputStream;
 import gui.Canvas;
 
 import com.erebos.engine.core.*;
 
 
-public class Driver extends EGame{
+public class Driver extends EGame
+{
 
 	public Driver() 
 	{
@@ -23,7 +27,7 @@ public class Driver extends EGame{
 			homeFolder.mkdir();
 	
 		copyNatives(homeFolder);
-		System.setProperty("natives", homeFolder.getAbsolutePath());
+		System.setProperty("org.lwjgl.librarypath", homeFolder.getAbsolutePath());
 		
 		Driver d = new Driver();
 		d.setTargetFPS(30);
@@ -32,9 +36,48 @@ public class Driver extends EGame{
 	
 	public static void copyNatives(File destDir)
 	{
-		
-		//Extract native.zip to destDir
 	
-	}
+		Driver.class.getClassLoader();
+		InputStream stream=ClassLoader.getSystemResourceAsStream("natives/natives.zip");
+		byte[] buffer = new byte[1024];
+
+		     try
+		     {
+		    	ZipInputStream zis = new ZipInputStream(stream);
+		    	ZipEntry ze = zis.getNextEntry();
+		    	while(ze!=null)
+		    	{
+		    	   String fileName = ze.getName();
+		           File newFile = new File(destDir + File.separator + fileName);
+		           if(ze.isDirectory())  //Make sure we make a directory if we run into one in case it exist or we'll get exceptions
+		           {
+		        	   new File(newFile.getParent()).mkdirs();
+		           }
+		           else
+		           {
+		        	FileOutputStream fos = new FileOutputStream(newFile);    
+		            new File(newFile.getParent()).mkdirs();
+		            fos = new FileOutputStream(newFile);             
+		            int len;
+		            
+		            while ((len = zis.read(buffer)) > 0) 
+		            {
+		            	fos.write(buffer, 0, len);
+		            }
+		            fos.close();   
+		           }
+		           ze = zis.getNextEntry();
+		    	}
+		        zis.closeEntry();
+		    	zis.close();
+		 
+		     }
+		     catch(IOException ex)
+		     {
+		    	 System.err.println("Error: Couldn't extract natives :"+ex.getMessage()); 
+		     }
+	 }    
+	
+	
 	
 }
