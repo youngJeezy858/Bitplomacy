9c39aeec586c77ace60f77577bba237a6ebf4f6c
Barry Martin <nyxcharon@gmail.com>
2013-10-17 14:21:42 -0400

    *Adding someframe work to finish up the native code

--- Bitplomacy/src/driver/Driver.java
+++ Bitplomacy/src/driver/Driver.java
@@ -1,4 +1,7 @@
 package driver;
+import java.io.File;
+import java.io.ObjectInputStream.GetField;
+
 import gui.Canvas;
 
 import com.erebos.engine.core.*;
@@ -6,16 +9,32 @@ import com.erebos.engine.core.*;
 
 public class Driver extends EGame{
 
-	public Driver() {
+	public Driver() 
+	{
 		super("Bitplomacy");
 		setShowFPS(true);
 		addCanvas(Canvas.getC());
 	}
 
-	public static void main(String[] args){
+	public static void main(String[] args)
+	{
+		File homeFolder=new File(System.getProperty("user.home")+File.separator+"Bitplomacy");
+		if(!homeFolder.exists())
+			homeFolder.mkdir();
+	
+		copyNatives(homeFolder);
+		System.setProperty("natives", homeFolder.getAbsolutePath());
+		
 		Driver d = new Driver();
 		d.setTargetFPS(30);
 		d.start(d, 1380, 831, false);
+	}
+	
+	public static void copyNatives(File destDir)
+	{
+		
+		//Extract native.zip to destDir
+	
 	}
 	
 }
