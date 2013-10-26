b2063c0340a9f8871ff16b306609e175a1fd242a
nyxcharon <nyxcharon@gmail.com>
2013-10-25 20:58:06 -0400

    *Added code to detect windows 32 and 64 bit and handle natives respectively

--- Bitplomacy/src/driver/Driver.java
+++ Bitplomacy/src/driver/Driver.java
@@ -43,17 +43,21 @@ public class Driver extends EGame
 		String osArch=System.getProperty("os.arch");
 		String osName=System.getProperty("os.name");
 		
+		//System.out.println(osArch+" "+osName);
+		
 		byte[] buffer = new byte[1024];
-
+		ZipInputStream zis=null;
 		     try
 		     {
-		    	ZipInputStream zis = new ZipInputStream(stream);
+		    	 zis = new ZipInputStream(stream);
 		    	ZipEntry ze = zis.getNextEntry();
 		    	while(ze!=null)
 		    	{
 		    	   String fileName = ze.getName();
 		    	   if((fileName.contains(".so") && osName.contains("inux")) || 
-		    		  ((fileName.contains(".jnilib")||fileName.contains(".dylib"))&&osName.contains("Mac")) )
+		    		  ((fileName.contains(".jnilib")||fileName.contains(".dylib"))&&osName.contains("Mac")) ||
+		    		  (!fileName.contains("64") && fileName.contains(".dll") && osName.contains("Win") && osArch.contains("86")) ||
+		    		  (fileName.contains(".dll") && fileName.contains("64") &&osName.contains("Win") &&osArch.contains("64")))
 		    	  {
 		           File newFile = new File(destDir + File.separator + fileName);
 		           if(ze.isDirectory())  //Make sure we make a directory if we run into one in case it exist or we'll get exceptions
@@ -63,15 +67,16 @@ public class Driver extends EGame
 		           else
 		           {
 		        	FileOutputStream fos = new FileOutputStream(newFile);    
-		            new File(newFile.getParent()).mkdirs();
-		            fos = new FileOutputStream(newFile);             
+		            new File(newFile.getParent()).mkdirs();             
 		            int len;
 		            
 		            while ((len = zis.read(buffer)) > 0) 
 		            {
 		            	fos.write(buffer, 0, len);
 		            }
+		            
 		            fos.close();   
+		            
 		           	} 
 		           }
 		           ze = zis.getNextEntry();
@@ -84,6 +89,7 @@ public class Driver extends EGame
 		     {
 		    	 System.err.println("Error: Couldn't extract natives :"+ex.getMessage()); 
 		     }
+		     
 	 }    
 	
 	
